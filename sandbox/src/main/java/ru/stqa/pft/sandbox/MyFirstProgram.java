faf13501a461112997fe1f547ab882901ddb451c
Anezhka <40922520+Anezhka@users.noreply.github.com>
2024-09-01 02:39:44 +0200

    Создан тест для создания группы. Исправлена ошибка testng в файле build.gradle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.annotations.Test;
+
 public class MyFirstProgram {
 
  	public static void main(String[] args) {
 
-
-		System.out.println("Hello, world!");
+		 System.out.println("Hello, world!");
 
 	}
 
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/TestForCheck.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+public class TestForCheck {
+
+  @Test
+  public void testForCheck() {
+    System.out.println("Hello, user! =)");
+  }
+
+
+
+
+}
