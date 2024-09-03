bea1d04fbbdc6bc328203640d8d67402d42c53c4
Anezhka <40922520+Anezhka@users.noreply.github.com>
2024-09-03 02:17:41 +0200

    Запуск тестов в разных браузерах.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,20 +1,32 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browserName;
 
-
+  public ApplicationManager(String browserName) {
+    this.browserName = browserName;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browserName == Browser.FIREFOX.browserName()) {
+      wd = new FirefoxDriver();
+    } else if (browserName == Browser.CHROME.browserName()) {
+      wd = new ChromeDriver();
+    } else if (browserName == Browser.IE.browserName()) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("https://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
@@ -22,8 +34,6 @@ public class ApplicationManager {
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
-
-
 
   public void stop() {
     wd.quit();
